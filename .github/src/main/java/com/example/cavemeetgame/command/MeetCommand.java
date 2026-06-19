1334a0600b50a582f1693174a3cc18e838fddc84
SkYuUuUuUuU <0203yuuto@gmail.com>
2026-06-20 00:04:33 +0900

    Create MeetCommand.java

---
+++ .github/src/main/java/com/example/cavemeetgame/command/MeetCommand.java
@@ -0,0 +1,28 @@
+package com.example.cavemeetgame.command;
+import com.example.cavemeetgame.manager.GameManager;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+public class MeetCommand implements CommandExecutor, TabCompleter {
+    private final GameManager gameManager;
+    public MeetCommand(GameManager gameManager) { this.gameManager = gameManager; }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (args.length == 0) return false;
+        String sub = args[0].toLowerCase();
+        if (sub.equals("start")) gameManager.startGame();
+        else if (sub.equals("stop")) gameManager.stopGame();
+        else if (sub.equals("reload")) gameManager.getPlugin().reloadConfig();
+        return true;
+    }
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
+        return args.length == 1 ? List.of("start", "stop", "leader", "reload", "monster") : new ArrayList<>();
+    }
+}
